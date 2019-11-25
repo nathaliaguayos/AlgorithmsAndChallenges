75d06f812e74a0cf88cf0a71e788a6c77b27f898
Nathali Aguayo <naguayo@intersysconsulting.com>
2019-11-25 14:53:16 -0600

    Adding a fibonacci solution using yield

---
+++ Algorithms/Fibonacci_Yield/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(int x in Fibonacci(10))
+            {
+                Console.WriteLine(x);
+            }
+        }
+        static IEnumerable<int> Fibonacci(int n)
+        {
+            for (int i=0, j=0, k=1;i<=n;i++)
+            {
+                yield return j;
+                int temp = j + k;
+                j = k;
+                k = temp;
+
+            }
+        }
+    }
+}
